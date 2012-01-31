afb568e23bdacf323478b7966fa3f86dc105d320
Jens Segers <segers.jens@gmail.com>
2012-01-31 13:59:40 +0100

    Demo installer controller + documentation tweaks

---
+++ controllers/install.php
@@ -0,0 +1,24 @@
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Install extends CI_Controller {
+    
+    /**
+     * This is a demo controller that allows you to add your first user account
+     * to the database, please remove this controller afterwards.
+     */
+    public function index() {
+        $this->load->library("auth");
+        $this->load->model("user_model");
+        
+        /* EDIT THESE FIELDS */
+        $user = array();
+        $user["username"] = "admin";
+        $user["password"] = "pass";
+        $user["email"] = "my@mail.com";
+        $user["activated"] = 1;
+        
+        $id = $this->user_model->insert($user);
+    }
+}
--- models/user_model.php
+++ models/user_model.php
@@ -1,37 +1,90 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class User_model extends CI_Model
-{
-	public function insert($user) {
-		$user['password'] = $this->auth->hash($user['password']);
-		$this->db->insert('users', $user);
-		return $this->db->insert_id();
-	}
-	
-	public function update($id, $user) {
-		if(isset($user['password']) && $user['password'])
-			$user['password'] = $this->auth->hash($user['password']);
-		else
-			unset($user['password']);
-			
-		$this->db->where('id', $id)->update('users', $user);
-		return $id;
-	}
-	
-	public function delete($id) {
-		$this->db->where('id', $id)->delete('users');
-	}
-	
-	public function get($key, $where='id') {
-		return $this->db->where($where, $key)->get('users')->row_array();
-	}
-	
-	public function activate($id) {
-		return $this->update($id, array('activated'=>1));
-	}
-	
-	public function deactivate($id) {
-		return $this->update($id, array('activated'=>0));
-	}
+/**
+ * This is an example user model, edit to match your implementation
+ */
+class User_model extends CI_Model {
+    
+    /**
+     * Add a user, password will be hashed
+     * 
+     * @param array user
+     * @return int id
+     */
+    public function insert($user) {
+        $user['password'] = $this->auth->hash($user['password']);
+        $user['registered'] = time();
+        
+        $this->db->insert('users', $user);
+        return $this->db->insert_id();
+    }
+    
+    /**
+     * Update a user, password will be hashed
+     * 
+     * @param int id
+     * @param array user
+     * @return int id
+     */
+    public function update($id, $user) {
+        if (isset($user['password']) && $user['password'])
+            $user['password'] = $this->auth->hash($user['password']);
+        else
+            unset($user['password']);
+        
+        $this->db->where('id', $id)->update('users', $user);
+        return $id;
+    }
+    
+    /**
+     * Delete a user
+     * 
+     * @param int key
+     * @param string identification field
+     */
+    public function delete($key, $where = 'id') {
+        $this->db->where($where, $key)->limit(1)->delete('users');
+    }
+    
+    /**
+     * Retrieve a user
+     * 
+     * @param int key
+     * @param string identification field
+     */
+    public function get($key, $where = 'id') {
+        $user = $this->db->where($where, $key)->get('users')->row_array();
+        return $user;
+    }
+    
+    /**
+     * Get a list of users with pagination options
+     * 
+     * @param int limit
+     * @param int offset
+     * @return array users
+     */
+    public function get_list($limit = null, $offset = null) {
+        return $this->db->order_by("name")->get("users")->limit($limit, $offset)->result_array();
+    }
+    
+    /**
+     * Activate a user
+     * 
+     * @param int id
+     */
+    public function activate($id) {
+        return $this->update($id, array('activated' => 1));
+    }
+    
+    /**
+     * Deactivate a user
+     * 
+     * @param int id
+     */
+    public function deactivate($id) {
+        return $this->update($id, array('activated' => 0));
+    }
 
 }
